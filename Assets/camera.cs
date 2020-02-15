4c049d83f5b39aa28478d8561c37718323b28347
Muhan Li <q4f2b@ugrad.cs.ubc.ca>
2020-02-15 04:32:53 -0800

    fitter player

---
+++ Assets/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject player;
+    public Transform trans;
+    public Transform myTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trans = player.GetComponent<Transform>();
+        myTrans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTrans.position = new Vector3(trans.position.x, trans.position.y, -10f);
+    }
+}
