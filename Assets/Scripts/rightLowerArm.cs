8461fcec47245e9d741906ea9f05aa7fb97e0548
Muhan Li <q4f2b@ugrad.cs.ubc.ca>
2020-02-15 18:06:33 -0800

    apply player skin

---
+++ Assets/Scripts/rightLowerArm.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightLowerArm : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public float force = 0;
+    private Collider2D myColl;
+    public Collider2D[] list = { };
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.freezeRotation = true;
+        myColl = GetComponent<Collider2D>();
+        foreach (Collider2D coll in list)
+        {
+            Physics2D.IgnoreCollision(myColl, coll);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("8"))
+        {
+            rigid.freezeRotation = false;
+            rigid.AddTorque(force, ForceMode2D.Force);
+        }
+        else if (Input.GetKey("i"))
+        {
+            rigid.freezeRotation = false;
+            rigid.AddTorque(-force, ForceMode2D.Force);
+        }
+        else
+        {
+            rigid.freezeRotation = true;
+        }
+    }
+}
