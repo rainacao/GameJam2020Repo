944fe4f3cc8dcfe83584581e1eb32fdb6bfbc84b
Raina Cao <45450525+rainacao@users.noreply.github.com>
2020-02-15 20:19:24 -0800

    wheat sprites and animation, script for falling obstacles

---
+++ Assets/moveObs.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveObs : MonoBehaviour
+{
+    private int startPoint = 20;
+    private int endPoint = -20;
+    public GameObject bolt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        bolt.transform.position = new Vector3(transform.position.x, startPoint, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bolt.transform.Translate(Vector3.down * 10 * Time.deltaTime);
+
+        if (bolt.transform.position.y <= endPoint)
+        {
+            bolt.transform.position = new Vector3(transform.position.x, startPoint, transform.position.z);
+        }
+    }
+}
